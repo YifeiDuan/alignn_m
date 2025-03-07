973dd6a6fb897c145294d863703c61c50a8e1a40
Yifei Duan <duanyf99@mit.edu>
2025-03-07 17:24:28 -0500

    transfer/features

--- transfer/features.py
+++ transfer/features.py
@@ -292,6 +292,7 @@ def find_subdirs_with_string(directory, search_str):
 
 
 if __name__ == "__main__":
+    print(args.database)
     logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S') 
     if args.database == "jarvis" or "mp":
         if args.prop != 'all':
