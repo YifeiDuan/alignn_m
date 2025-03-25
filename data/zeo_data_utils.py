5584207afb9266fcd0fcf091e0ae66ef1a963750
Yifei Duan <duanyf99@mit.edu>
2025-03-25 13:02:22 -0400

    zeo initial commit

--- data/zeo_data_utils.py
+++ data/zeo_data_utils.py
@@ -7,15 +7,17 @@ import numpy as np
 
 def download_cif_from_url(
         url_dir="https://raw.githubusercontent.com/marko-petkovic/zeolite-property-prediction/refs/heads/main/Data_raw/output1",
-        struc_dir="zeo_data/zeolite-property"
+        struc_dir="zeo_data/zeolite-property",
+        code="MOR_100"
         ):
     if not os.path.exists(struc_dir):
         os.makedirs(struc_dir)
 
-    file_name = "MOR_100.cif"
+    file_name = f"{code}.cif"
     url_subdir = file_name.split("_")[1].split(".")[0]
 
     url = os.path.join(os.path.join(url_dir, url_subdir), file_name)
     file_path = os.path.join(struc_dir, file_name)
 
     wget.download(url, file_path)
+
