0897c2125331c99e07388f6e221e63c078ef2e6a
Yifei Duan <duanyf99@mit.edu>
2025-10-11 17:47:04 -0400

    features zeo_text

--- transfer/run_tl_zeo.py
+++ transfer/run_tl_zeo.py
@@ -57,7 +57,7 @@ def run_regressor_rf(args):
     df_base_name = f"dataset_{args.gnn}_{args.llm}_{args.text}_prop_{session_name}"
     if "text_only" in session_name:
         substring = session_name.split("text_only_")[-1]
-        df_base_name = f"dataset_{args.gnn}_{args.llm}_{args.text}_prop_{substring}"
+        df_base_name = f"dataset_{args.llm}_{args.text}_prop_{substring}"
     df_train = pd.read_csv(os.path.join(data_dir, f"{df_base_name}_train.csv")).reset_index(drop=True)
     df_val   = pd.read_csv(os.path.join(data_dir, f"{df_base_name}_val.csv")).reset_index(drop=True)
     df_test  = pd.read_csv(os.path.join(data_dir, f"{df_base_name}_test.csv")).reset_index(drop=True)
@@ -149,7 +149,7 @@ def run_regressor_mlp(args):
     df_base_name = f"dataset_{args.gnn}_{args.llm}_{args.text}_prop_{session_name}"
     if "text_only" in session_name:
         substring = session_name.split("text_only_")[-1]
-        df_base_name = f"dataset_{args.gnn}_{args.llm}_{args.text}_prop_{substring}"
+        df_base_name = f"dataset_{args.llm}_{args.text}_prop_{substring}"
     df_train = pd.read_csv(os.path.join(data_dir, f"{df_base_name}_train.csv")).reset_index(drop=True)
     df_val   = pd.read_csv(os.path.join(data_dir, f"{df_base_name}_val.csv")).reset_index(drop=True)
     df_test  = pd.read_csv(os.path.join(data_dir, f"{df_base_name}_test.csv")).reset_index(drop=True)
