a76dc5b877948834f83e5ef789931bbd25ec3b60
Yifei Duan <duanyf99@mit.edu>
2025-11-12 17:21:25 -0500

    tag

--- tag/train_zeo_tag.py
+++ tag/train_zeo_tag.py
@@ -10,7 +10,6 @@ import sys
 import json
 import zipfile
 from alignn.data import get_train_val_loaders
-from ZeoliteData import get_data_alignn
 from alignn.train import train_dgl_prop
 from alignn.config import TrainingConfig
 from jarvis.db.jsonutils import loadjson
