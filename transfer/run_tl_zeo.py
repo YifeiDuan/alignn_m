903bfa1f04e104103077598eded5bfb8f54a371e
Yifei Duan <duanyf99@mit.edu>
2025-11-25 13:33:10 -0500

    preprocess update

--- transfer/run_tl_zeo.py
+++ transfer/run_tl_zeo.py
@@ -62,7 +62,7 @@ def run_regressor_rf(args):
         substring = session_name.split("randtext_")[-1]
         df_base_name = f"dataset_{args.llm}_randtext_prop_{substring}"
     if "input" in args.text:    # LLM generated text
-        df_base_name = f"dataset_{args.gnn}_{args.text}_prop_{substring}"
+        df_base_name = f"dataset_{args.gnn}_{args.text}_prop_{session_name}"
     df_train = pd.read_csv(os.path.join(data_dir, f"{df_base_name}_train.csv")).reset_index(drop=True)
     df_val   = pd.read_csv(os.path.join(data_dir, f"{df_base_name}_val.csv")).reset_index(drop=True)
     df_test  = pd.read_csv(os.path.join(data_dir, f"{df_base_name}_test.csv")).reset_index(drop=True)
@@ -159,7 +159,7 @@ def run_regressor_mlp(args):
         substring = session_name.split("randtext_")[-1]
         df_base_name = f"dataset_{args.llm}_randtext_prop_{substring}"
     if "input" in args.text:    # LLM generated text
-        df_base_name = f"dataset_{args.gnn}_{args.text}_prop_{substring}"
+        df_base_name = f"dataset_{args.gnn}_{args.text}_prop_{session_name}"
     df_train = pd.read_csv(os.path.join(data_dir, f"{df_base_name}_train.csv")).reset_index(drop=True)
     df_val   = pd.read_csv(os.path.join(data_dir, f"{df_base_name}_val.csv")).reset_index(drop=True)
     df_test  = pd.read_csv(os.path.join(data_dir, f"{df_base_name}_test.csv")).reset_index(drop=True)
